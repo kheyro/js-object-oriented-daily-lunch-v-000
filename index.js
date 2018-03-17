841ddb8e310f24045f8a551a952334d86fd3d6b1
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-17 10:54:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ let deliveryId = 0, customerId = 0
 
 class Customer {
   constructor(name, employer) {
+    this.id = ++customerId
     this.name = name
     this.employer = emloyer.id
+    store.customers.push(this)
   }
+
+  
 }
