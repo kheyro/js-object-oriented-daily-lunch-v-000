df1e5db17181d3ba8eccbdb9572e780fd1024631
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-17 10:51:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 let store = { deliveries: [], meals: [], employers: [], customers: []}
+let deliveryId = 0
+
+class Delivery {
+
+  contructor(name) {
+    this.name = name
+    this.id = deliveryId++
+  }
+  
+}
