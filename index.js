9685e82491e42ef94db0365bcfe8cebf7f08d59a
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-17 11:17:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ class Customer {
   constructor(name, employer) {
     this.id = ++customerId
     this.name = name
-    this.employerId = employer.id
+    if (employer) {
+      this.employerId = employer.id
+    }
     store.customers.push(this)
   }
 
